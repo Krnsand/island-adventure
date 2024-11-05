4fc7be49601d1e5448465c64ea3ad42779eeb503
Krnsand <krnsandin@gmail.com>
2024-11-05 13:29:25 +0100

    Add assets folder

---
+++ assets/js/main.js
@@ -0,0 +1,114 @@
+window.addEventListener("DOMContentLoaded", main);
+
+function main() {
+  loadStartingScene();
+}
+
+function loadStartingScene() {
+  //...  onclick.
+}
+
+function loadEnterJungle() {
+  //...
+}
+
+function loadStayOBeach() {
+  //...
+}
+function loadHelloMonkey() {
+  //...
+}
+function loadAskForDirections() {
+  //...
+}
+
+function loadGoRight() {
+  //...
+}
+function loadGoLeft() {
+  //...
+}
+
+function loadYouFoundWater() {
+  //...
+}
+function loadYouFoundAHole() {
+  //...
+}
+
+function loadHelloParrot() {
+  //...
+}
+function loadAskForFood() {
+  //...
+}
+function loadPickBerries() {
+  //...
+}
+function loadClimbForCoconuts() {
+  //...
+}
+function loadGoBackSameWay() {
+  //...
+}
+function loadExploreMore() {
+  //...
+}
+function loadPickUpBranches() {
+  //...
+}
+function loadMakeShelter() {
+  //...
+}
+function loadMakeFire() {
+  //...
+}
+function loadSleepInCold() {
+  //...
+}
+function loadHelloCrab() {
+  //...
+}
+function loadHelpCrab() {
+  //...
+}
+function loadNoHelpCrab() {
+  //...
+}
+function loadPickUpShell() {
+  //...
+}
+function loadPutShellDown() {
+  //...
+}
+function loadSeBoat() {
+  //...
+}
+function loadGetRescued() {
+  //...
+}
+
+
+/*Kod innan function loadSceneContent och loadLeftButton och loadRightButton:
+function scene2WatchingTv() {
+ 
+    function loadSceneContent(img, headline, paragraph) {
+    const image = document.getElementById("background-img");
+    image.src = img;
+ 
+    const h2 = document.getElementById("scene-headline");
+    h2.textContent = headline;
+ 
+    const p = document.getElementById("scene-paragraph");
+    p.textContent = paragraph;
+ 
+    const buttonLeft = document.getElementById("buttonLeft");
+    buttonLeft.onclick = function enterScene4Collapse() {
+        scene4Collapse();
+    };
+ 
+    const buttonRight = document.getElementById("buttonRight");
+    buttonRight.onclick = function enterScene3Rabbithole() {
+        scene3Rabbithole();
+    };
+    */
